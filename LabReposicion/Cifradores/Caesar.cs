353debd7a90b65134ea6cb403f6e6b564d96789b
EdgarP43 <e.paredes-cas@outlook.com>
2020-05-01 12:20:13 -0600

    Laboratorio cifrado asimetrico

---
+++ LabReposicion/Cifradores/Caesar.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LabReposicion.Cifradores
+{
+    public class Caesar
+    {
+        public string AbecedarioOriginal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.-{}´+'¿|<>;:_[]¨*°?¡ !\"#$%&/()=+\r" + (char)10;
+        public string AbecesarioCifrado = "";
+        public List<string> ListaOriginal = new List<string>();
+        public List<string> ListaCif = new List<string>();
+
+        public void ArmarNuevoDic(int clave)
+        {
+            for (int i = clave; i < AbecedarioOriginal.Length; i++)
+            {
+                AbecesarioCifrado += AbecedarioOriginal[i];
+            }
+            var corrido = AbecedarioOriginal.Length - AbecesarioCifrado.Length;
+            for (int i = 0; i < corrido; i++)
+            {
+                AbecesarioCifrado += AbecedarioOriginal[i];
+            }
+        }
+
+        public string CifrarCesar(string Texto)
+        {
+            for (int i = 0; i < AbecedarioOriginal.Length; i++)
+            {
+                ListaOriginal.Add(AbecedarioOriginal[i].ToString());
+            }
+            for (int i = 0; i < AbecesarioCifrado.Length; i++)
+            {
+                ListaCif.Add(AbecesarioCifrado[i].ToString());
+            }
+            var cif = ""; var listapos = new List<int>();
+            for (int i = 0; i < Texto.Length; i++)
+            {
+                listapos.Add(ListaOriginal.IndexOf(Texto[i].ToString()));
+            }
+            foreach (var item in listapos)
+            {
+                cif += AbecesarioCifrado[item].ToString();
+            }
+            return cif;
+        }
+
+        public string DescifrarCesar(string texto)
+        {
+            for (int i = 0; i < AbecesarioCifrado.Length; i++)
+            {
+                ListaCif.Add(AbecesarioCifrado[i].ToString());
+            }
+            var listaposdesc = new List<int>(); var descif = "";
+            for (int i = 0; i < texto.Length; i++)
+            {
+                listaposdesc.Add(ListaCif.IndexOf(texto[i].ToString()));
+            }
+            foreach (var item in listaposdesc)
+            {
+                descif += AbecedarioOriginal[item].ToString();
+            }
+            return descif;
+        }
+
+        public void EscribirTextoParaCifrar(string texto, string ruta, string Llave)
+        {
+            using (var file = new FileStream(ruta, FileMode.OpenOrCreate))
+            {
+                using (var escritor = new StreamWriter(file))
+                {
+                    escritor.Write(texto);
+                    escritor.WriteLine("\n");
+                    escritor.WriteLine("$LLaveCifrada: " + Llave);
+                }
+            }
+        }
+
+        public void EscribirTextoDescifrado(string texto, string ruta)
+        {
+            using (var file = new FileStream(ruta, FileMode.OpenOrCreate))
+            {
+                using (var escritor = new StreamWriter(file))
+                {
+                    escritor.Write(texto);
+                }
+            }
+        }
+
+        public string CargarArchivo(string ruta)
+        {
+            var txt = "";
+            using (var file = new FileStream(ruta, FileMode.OpenOrCreate))
+            {
+                using (var lectro = new StreamReader(file))
+                {
+                    txt = lectro.ReadToEnd();
+                }
+            }
+            return txt;
+        }
+    }
+}
---
+++ LabReposicion/Controllers/RSACesarController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Web;
+using System.Text;
+using LabReposicion.Cifradores;
+
+namespace LabReposicion.Controllers
+{
+    public class RSAData
+    {
+        public string RutaArchivo { get; set; }
+        public int claveCesar { get; set; }
+        public int[] Kpriv { get; set; }
+    }
+    public class ArchivoData
+    {
+        public string NombreArchivo { get; set; }
+    }
+    [Produces("application/json")]
+    [Route("api/RSACesar")]
+    public class RSACesarController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("cipher/getPublicKey")]
+        public ActionResult<IEnumerable<string>> Getcesar([FromBody] object Cifrar)
+        {
+            var a = JsonConvert.SerializeObject(Cifrar);
+            ArchivoData rsa = JsonConvert.DeserializeObject<ArchivoData>(a);
+            var text = "Cipher: RSA";
+            RSA modelo = new RSA();
+            Random azar = new Random();
+            var p = azar.Next(1, 100);
+            var primo = modelo.esPrimoV2(p);
+            while (!primo)
+            {
+                p = azar.Next(1, 100);
+                primo = modelo.esPrimoV2(p);
+            }
+            var q = azar.Next(1, 100);
+            primo = modelo.esPrimoV2(q);
+            while (!primo)
+            {
+                q = azar.Next(1, 100);
+                primo = modelo.esPrimoV2(q);
+            }
+            var N = modelo.N(p, q);
+            var øN = modelo.øN(p, q);
+            var Euler = modelo.EncontrarEuler(øN, N);
+            var D = modelo.EncontrarD(Euler, øN);
+            var Kpub = modelo.ReceptorKpub(Euler, N);
+            var Kpriv = modelo.ReceptorKpriv(D, N);
+            var vec = rsa.NombreArchivo.Split(".");
+            var nombreArch = vec[0];
+            var rutaLlaves = Path.GetFullPath("Llaves\\" + nombreArch + ".Keys");
+            var listaAux = new List<int[]>();
+            listaAux.Add(Kpub);
+            listaAux.Add(Kpriv);
+            modelo.EscribirLLaves(listaAux, rutaLlaves);
+            var text2 = "Key: " + Kpub[0].ToString() + "," + Kpub[1].ToString();
+            return new string[] { text, text2 };
+        }
+
+        // POST api/values
+        [Route("cipher/caesar2")]
+        [HttpPost]
+        public void Cifrar([FromBody] object Cifrar)
+        {
+            var a = JsonConvert.SerializeObject(Cifrar);
+            RSAData rsa = JsonConvert.DeserializeObject<RSAData>(a);
+            var vec = rsa.RutaArchivo.Split("/");
+            var vec2 = vec[vec.Length - 1].Split(".");
+            var nombre = vec2[0];
+            var rutaCif = Path.GetFullPath("Cifrados\\" + vec[vec.Length - 1]);
+            var rutaLlaves = Path.GetFullPath("Llaves\\" + nombre + ".Keys");
+            RSA modelo = new RSA();
+            var Kpub = modelo.LecturaLLaves(rutaLlaves)[0];
+            var Cifrado = modelo.FormulazoCifrado(Kpub, rsa.claveCesar);
+            Caesar cesar = new Caesar();
+            cesar.ArmarNuevoDic(rsa.claveCesar);
+            var textoPlano = cesar.CargarArchivo(rsa.RutaArchivo);
+            var textoCifrado = cesar.CifrarCesar(textoPlano);
+            cesar.EscribirTextoParaCifrar(textoCifrado, rutaCif, Cifrado);
+        }
+
+        [Route("descipher/Caesar2")]
+        [HttpPost]
+        public void Descifrar([FromBody] object Cifrar)
+        {
+            var a = JsonConvert.SerializeObject(Cifrar);
+            RSAData rsa = JsonConvert.DeserializeObject<RSAData>(a);
+            var vec = rsa.RutaArchivo.Split("/");
+            var vec2 = vec[vec.Length - 1].Split(".");
+            var nombre = vec2[0];
+            var rutaDescif = Path.GetFullPath("Descifrados\\" + vec[vec.Length - 1]);
+            var rutaLlaves = Path.GetFullPath("Llaves\\" + nombre + ".Keys");
+            RSA modelo = new RSA();
+            var kpriv = modelo.LecturaLLaves(rutaLlaves)[1];
+            Caesar cesar = new Caesar();
+            cesar.ArmarNuevoDic(rsa.claveCesar);
+            var textoPlano = cesar.CargarArchivo(rsa.RutaArchivo);
+            var textoCifrado = cesar.DescifrarCesar(textoPlano);
+            cesar.EscribirTextoDescifrado(textoCifrado, rutaDescif);
+        }
+
+    }
+}
